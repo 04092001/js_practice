8a88a8cfc7e0c102341705943b0280d31de0d745
Freddie Heath <fheath090401@gmail.com>
2023-12-23 15:53:06 +0000

    Rewriting most of the functions as arrow functions

--- src3/script.js
+++ src3/script.js
@@ -1,27 +1,16 @@
 // Write a function called add7 that takes one number and returns that number + 7.
-function add7(number) {
-  newNum = number + 7;
-  return newNum;
-}
+let add7 = (number) => number + 7;
 
-add7(1);
-console.log(newNum);
+console.log(add7(2));
 
 // Write a function called multiply that takes 2 numbers and returns their product.
-function multiply(x, y) {
-  number = x * y;
-  return number;
-}
+let multiply = (x, y) => x * y;
 
-multiply(2, 4);
-console.log(number);
+console.log(multiply(3, 4));
 
 // Write a function called capitalize that takes a string and returns that string with only the first letter capitalized. Make sure that it can take strings that are lowercase, UPPERCASE or BoTh
-function capitalise(inputString) {
-  return (
-    inputString.charAt(0).toUpperCase() + inputString.slice(1).toLowerCase()
-  );
-}
+let capitalise = (inputString) =>
+  inputString.charAt(0).toUpperCase() + inputString.slice(1).toLowerCase();
 
 let stringOne = "hello world!";
 console.log(capitalise(stringOne));
@@ -33,13 +22,20 @@ let stringThree = "a MiXturE of UppER CasE and LOWeR cAsE";
 console.log(capitalise(stringThree));
 
 // Write a function called lastLetter that takes a string and returns the very last letter of that string: lastLetter("abcd") should return "d"
-
-function lastLetter(inputWord) {
-  return inputWord.slice(-1);
-}
+let lastLetter = (inputWord) => inputWord.slice(-1);
 
 let wordOne = "Manchester";
-console.log(lastLetter(wordOne));
-
 let wordTwo = "Guardiola";
+
+console.log(lastLetter(wordOne));
 console.log(lastLetter(wordTwo));
+
+function lastTwoLetters(word) {
+  return word.slice(-2);
+}
+
+let example1 = "De Bruyne";
+let example2 = "Haaland";
+
+console.log(lastTwoLetters(example1));
+console.log(lastTwoLetters(example2));
