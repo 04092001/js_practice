ef1c2aa5a34bbcbaae9869876afe11a232cfbc2b
Freddie Heath <fheath090401@gmail.com>
2023-12-23 13:50:10 +0000

    Experimenting with creating functions

--- src3/script.js
+++ src3/script.js
@@ -17,5 +17,29 @@ multiply(2, 4);
 console.log(number);
 
 // Write a function called capitalize that takes a string and returns that string with only the first letter capitalized. Make sure that it can take strings that are lowercase, UPPERCASE or BoTh
+function capitalise(inputString) {
+  return (
+    inputString.charAt(0).toUpperCase() + inputString.slice(1).toLowerCase()
+  );
+}
+
+let stringOne = "hello world!";
+console.log(capitalise(stringOne));
+
+let stringTwo = "goodbye World!";
+console.log(capitalise(stringTwo));
+
+let stringThree = "a MiXturE of UppER CasE and LOWeR cAsE";
+console.log(capitalise(stringThree));
 
 // Write a function called lastLetter that takes a string and returns the very last letter of that string: lastLetter("abcd") should return "d"
+
+function lastLetter(inputWord) {
+  return inputWord.slice(-1);
+}
+
+let wordOne = "Manchester";
+console.log(lastLetter(wordOne));
+
+let wordTwo = "Guardiola";
+console.log(lastLetter(wordTwo));
