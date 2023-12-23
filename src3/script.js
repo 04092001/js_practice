7d499489b59b34ce922f5b1bd4c374fd8e807fc8
Freddie Heath <fheath090401@gmail.com>
2023-12-23 13:15:39 +0000

    Doing Odin Project Javascript fundamentals part 3 practice

--- src3/script.js
+++ src3/script.js
@@ -1,2 +1,21 @@
-const input = document.querySelector("input");
-const para = document.querySelector("p");
+// Write a function called add7 that takes one number and returns that number + 7.
+function add7(number) {
+  newNum = number + 7;
+  return newNum;
+}
+
+add7(1);
+console.log(newNum);
+
+// Write a function called multiply that takes 2 numbers and returns their product.
+function multiply(x, y) {
+  number = x * y;
+  return number;
+}
+
+multiply(2, 4);
+console.log(number);
+
+// Write a function called capitalize that takes a string and returns that string with only the first letter capitalized. Make sure that it can take strings that are lowercase, UPPERCASE or BoTh
+
+// Write a function called lastLetter that takes a string and returns the very last letter of that string: lastLetter("abcd") should return "d"
